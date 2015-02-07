75a41402dc77869d8f336005e51f203fedca053a
Daniel Boline <daniel.boline@stonybrook.edu>
2015-02-07 08:55:39 -0500

    ...

--- test_cloud9.sh
+++ test_cloud9.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
+./tests/garmin_app_unittests.py
 ./garmin.py get
 ./garmin.py sync
 ./garmin.py year run
-./garmin.py 2014-11-22_18 plot
-./unittests.py
-./running_pace_plot.py
-./world_record.py
+./garmin.py 2014-11-22_18
