005b990e42eab5bbd9d456c35f2145491308cbb1
Daniel Boline <daniel.boline@stonybrook.edu>
2015-02-28 09:18:33 -0500

    ...

--- garmin_app/garmin_cache.py
+++ garmin_app/garmin_cache.py
@@ -12,12 +12,6 @@
 '''
 
 import os
-import gzip
-
-try:
-    import cPickle as pickle
-except ImportError:
-    import pickle
 
 import pandas as pd
 
@@ -30,42 +24,23 @@ from garmin_app.garmin_utils import get_md5
 
 class GarminCache(object):
     ''' class to manage caching objects '''
-    def __init__(self, pickle_file='', cache_directory=''):
-        self.pickle_file = pickle_file
-        self.cache_directory = cache_directory
+    def __init__(self, cache_directory=''):
         self.cache_summary_list = []
         self.cache_summary_md5_dict = {}
         self.cache_summary_file_dict = {}
-        self.pickle_file_is_modified = False
-        self.do_update = False
-        if cache_directory:
-            if not os.path.exists(cache_directory):
-                os.makedirs(cache_directory)
-    
-    def read_pickle_object_in_file(self, pickle_file=''):
-        ''' read python object from gzipped pickle file '''
-        if not pickle_file:
-            if not self.pickle_file:
-                return None
-            else:
-                pickle_file = self.pickle_file
-        outobj = None
-        if os.path.exists(pickle_file):
-            with gzip.open(pickle_file, 'rb') as pkl_file:
-                outobj = pickle.load(pkl_file)
-        return outobj
         
-    def write_pickle_object_to_file(self, inpobj, pickle_file=''):
-        ''' write python object to gzipped pickle file '''
-        if not pickle_file:
-            if not self.pickle_file:
-                return False
-            else:
-                pickle_file = self.pickle_file
-        with gzip.open('%s.tmp' % pickle_file, 'wb') as pkl_file:
-            pickle.dump(inpobj, pkl_file, pickle.HIGHEST_PROTOCOL)
-        run_command('mv %s.tmp %s' % (pickle_file, pickle_file))
-        return True
+
+    def get_cached_object_in_file(self, fname='', garmin_class=None):
+        if not os.path.exists(fname) or not garmin_class:
+            return False
+        else:
+            return True
+
+    def write_cached_object_to_file(self, inobj=None, outfile=None):
+        if not inobj or not os.path.exists(outfile):
+            return False
+        else:
+            return True
 
     def read_cached_gfile(self, gfbasename=''):
         if not gfbasename or not self.cache_directory:
