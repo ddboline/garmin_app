779472cdc4862e20bd61f23afb529d90ad5d9263
Daniel Boline <daniel.boline@stonybrook.edu>
2015-03-03 09:01:45 -0500

    ...

--- tests/garmin_app_unittests.py
+++ tests/garmin_app_unittests.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 
 ''' unittests... '''
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
 
 import os
 import unittest
