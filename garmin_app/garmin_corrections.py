0394caa5be25c9538c6b1000018376cc8ac2174e
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-10 14:58:47 -0400

    ...

--- garmin_app/garmin_corrections.py
+++ garmin_app/garmin_corrections.py
@@ -288,5 +288,6 @@ list_of_corrected_laps = {
      '2015-04-28T11:12:37Z': {0: 6.5},
      '2015-04-30T15:13:26Z': {0: 6.5},
      '2015-05-02T11:01:04Z': {0: [50e3/METERS_PER_MILE, 29595]},
+     '2015-05-09T11:29:54Z': {0: 50e3/METERS_PER_MILE},
      'DUMMY': {0:0.0},}
     # 'time_string': {lap_no: [dist, time_in_sec]},
