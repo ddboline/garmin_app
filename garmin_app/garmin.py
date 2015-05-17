d4c5a1f7f96292e06dc8f859420e339e7706cee1
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-17 09:17:17 -0400

    ...

--- garmin_app/garmin.py
+++ garmin_app/garmin.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
-    Main module
+Created on Sun May 17 06:57:32 2015
+
+@author: ddboline
 """
 from __future__ import print_function
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from garmin_app.garmin_utils import garmin_arg_parse
+from garmin_utils import garmin_arg_parse
 
 if __name__ == '__main__':
     garmin_arg_parse()
