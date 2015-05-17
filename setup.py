e640e9bc1f0ed3a51b696ca5ecc34ee98cb41f95
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-17 10:01:51 -0400

    ...

--- setup.py
+++ setup.py
@@ -24,6 +24,6 @@ setup(
 #    install_requires=['pandas >= 0.13.0', 'numpy >= 1.8.0'],
     packages=['garmin_app'],
     package_dir={'garmin_app': 'garmin_app'},
-    package_data={'garmin_app': glob('templates/*.html')},
+    package_data={'garmin_app': ['templates/*.html']},
     scripts=['garmin.py']
 )
