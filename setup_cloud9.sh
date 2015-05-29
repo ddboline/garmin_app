8e208047d2865c0fd5b4e0f90632b0aaddd7f24f
Daniel Boline <daniel.boline@stonybrook.edu>
2015-05-29 17:19:57 -0400

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+sudo cp -a ~/.ssh /root/
+sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/pip_py2deb ./ > /etc/apt/sources.list.d/py2deb2.list"
 sudo apt-get update
-sudo apt-get install -y gpsbabel garmin-forerunner-tools xml2
-sudo apt-get install -y python-pandas python-dateutil
-sudo apt-get install -y python-usb
+
+sudo apt-get update
+sudo apt-get install -y --force-yes gpsbabel garmin-forerunner-tools xml2 python-requests \
+                                    python-pandas python-dateutil python-usb
 
 if [ -z $1 ] ; then
     true
