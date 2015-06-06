7b497c35ae3a121b879bca30f29f6867c2691542
Daniel Boline <daniel.boline@stonybrook.edu>
2015-06-06 14:25:00 -0400

    ...

--- garmin_app/garmin_corrections.py
+++ garmin_app/garmin_corrections.py
@@ -66,7 +66,7 @@ def list_of_corrected_laps(json_path=None):
         json_path = '%s/garmin_app/garmin_app' % os.getenv('HOME')
     if len(_list_of_corrected_laps) == 0:
         with open('%s/garmin_corrections.json' % json_path, 'rb') as jfile_:
-            tmp_dict = json.load(jfile_)
+            tmp_dict = json.loads(jfile_.read())
             for key, val in tmp_dict.items():
                 tmp_ = {}
                 for k2_, v2_ in val.items():
