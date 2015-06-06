c8eac653a68f8485d55e5a4bcd68504035a14d49
Daniel Boline <daniel.boline@stonybrook.edu>
2015-06-06 13:54:11 -0400

    ...

---
+++ setup_conda.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y gpsbabel garmin-forerunner-tools xml2
+sudo /opt/conda/bin/conda install --yes requests pandas dateutil usb
+
+if [ -z $1 ] ; then
+    true
+elif [ $1 = "html" ] ; then
+    CURDIR=`pwd`
+    cd ${HOME}
+    git clone -b CLOUDVERSION https://github.com/ddboline/ddboline_html.git public_html
+    cd public_html/
+    sh setup_cloud9.sh
+    mkdir garmin
+    cd $CURDIR;
+fi
+
+### This is a bit of a hack...
+if [ $USER = "root" ] ; then
+    echo "America/New_York" > timezone
+    sudo mv timezone /etc/timezone
+    sudo rm /etc/localtime
+    sudo ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime;
+fi
