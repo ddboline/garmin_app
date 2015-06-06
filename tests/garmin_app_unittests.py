534aadc67f028b6289312abbe0fdab5ef046abe7
Daniel Boline <daniel.boline@stonybrook.edu>
2015-06-06 15:32:39 -0400

    ...

--- tests/garmin_app_unittests.py
+++ tests/garmin_app_unittests.py
@@ -253,6 +253,7 @@ class TestGarminApp(unittest.TestCase):
         gsum = garmin_summary.GarminSummary(FITFILE)
         gsum.read_file()
         output = gsum.__repr__()
+        print(output.encode())
         mstr = hashlib.md5()
         mstr.update(output.encode())
         self.assertEqual(mstr.hexdigest(), 'f73293da4f5d64545ad4a1d5a1efb283')
