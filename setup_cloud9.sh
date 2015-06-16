7dad037abb809d0368c336ff374bf06c7396fffc
Daniel Boline <daniel.boline@stonybrook.edu>
2015-06-16 19:00:46 -0400

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-sudo cp -a ~/.ssh /root/
+sudo cp -a ${HOME}/.ssh /root/
+sudo chown -R root:root /root/
 sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/pip_py2deb ./ > /etc/apt/sources.list.d/py2deb2.list"
-sudo apt-get update
-
 sudo apt-get update
 sudo apt-get install -y --force-yes gpsbabel garmin-forerunner-tools xml2 python-requests \
                                     python-pandas python-dateutil python-usb
