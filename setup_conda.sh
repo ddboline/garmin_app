4482286cbe49127ffa938ca1ac4b056aae0f3d3f
Daniel Boline <daniel.boline@stonybrook.edu>
2015-07-06 15:05:01 -0400

    install fit2tcx from private repository

--- setup_conda.sh
+++ setup_conda.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+sudo cp -a ${HOME}/.ssh /root/
+sudo chown -R root:root /root/
+sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/pip_py2deb ./ > /etc/apt/sources.list.d/py2deb2.list"
 sudo apt-get update
-sudo apt-get install -y gpsbabel garmin-forerunner-tools xml2
+sudo apt-get install -y --force-yes gpsbabel garmin-forerunner-tools xml2 fit2tcx
 sudo /opt/conda/bin/conda install --yes pip requests pandas dateutil matplotlib
 
 sudo /opt/conda/bin/pip install --upgrade pyusb
