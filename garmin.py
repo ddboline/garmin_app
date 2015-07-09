87c0778377971b385725edfca8909e4889e3d76d
Daniel Boline <ddboline@gmail.com>
2015-07-09 13:15:28 -0400

    script_path

--- garmin.py
+++ garmin.py
@@ -8,7 +8,8 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import garmin_app
 from garmin_app.garmin_utils import garmin_arg_parse
 
 if __name__ == '__main__':
-    garmin_arg_parse()
+    garmin_arg_parse(script_path=garmin_app.__path__[0])
