3d8666a85c7719268fde74d7a4e58263802994bb
Daniel Boline <ddboline@gmail.com>
2015-07-29 17:29:58 -0400

    ...

--- garmin_app/garmin_daemon.py
+++ garmin_app/garmin_daemon.py
@@ -8,7 +8,6 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import os
 import multiprocessing
 from .util import OpenUnixSocketServer, OpenSocketConnection
 
