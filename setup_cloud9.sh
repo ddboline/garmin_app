92358f02de95bd73c2afba70ba5bc083f2f9008d
Daniel Boline <ddboline@gmail.com>
2015-08-13 17:37:17 -0400

    sqlalchemy

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -5,7 +5,8 @@ sudo chown -R root:root /root/
 sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/pip_py2deb ./ > /etc/apt/sources.list.d/py2deb2.list"
 sudo apt-get update
 sudo apt-get install -y --force-yes gpsbabel garmin-forerunner-tools xml2 python-requests \
-                                    python-pandas python-dateutil python-usb fit2tcx python-psycopg2
+                                    python-pandas python-dateutil python-usb fit2tcx \
+                                    python-psycopg2 python-sqlalchemy
 
 if [ -z $1 ] ; then
     true
