32d47bae9ee4383ed1da0407b21d8a7c1b356a57
Daniel Boline <ddboline@gmail.com>
2015-09-11 10:47:26 -0400

    write_postgresql_table

--- garmin_app/garmin_cache_sql.py
+++ garmin_app/garmin_cache_sql.py
@@ -9,17 +9,18 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 from .garmin_cache import GarminCache
-from .garmin_summary import GarminSummary
-
+from .garmin_summary import GarminSummary, DB_ENTRIES
 from sqlalchemy import (create_engine, Column, Integer, Float, String,
                         DateTime)
 from sqlalchemy.ext.declarative import declarative_base
-
 from sqlalchemy.orm import sessionmaker
+
+from .util import OpenPostgreSQLsshTunnel, POSTGRESTRING
 
 Base = declarative_base()
 
 class GarminSummaryTable(Base):
+    """ ORM class for garmin_summary table """
     __tablename__ = 'garmin_summary'
 
     filename = Column(String, primary_key=True)
@@ -35,10 +36,11 @@ class GarminSummaryTable(Base):
 
     def __repr__(self):
         return 'GarminSummaryTable<%s>' % ', '.join(
-            '%s=%s' % (x, getattr(self, x)) for x in GarminSummary._db_entries)
+            '%s=%s' % (x, getattr(self, x)) for x in DB_ENTRIES)
 
 
-class GarminCacheSQL:
+class GarminCacheSQL(object):
+    """ cache in SQL database using sqlalchemy """
     def __init__(self, sql_string='', pickle_file='', cache_directory='',
                  corr_list=None, garmin_cache=None, summary_list=None):
         if garmin_cache is not None:
@@ -60,38 +62,44 @@ class GarminCacheSQL:
         self.create_table()
 
     def create_table(self):
+        """ create table """
         Base.metadata.create_all(self.engine)
 
     def delete_table(self):
+        """ drop table """
         Base.metadata.drop_all(self.engine)
 
     def read_sql_table(self):
-        Session = sessionmaker(bind=self.engine)
-        session = Session()
+        """ deserialize from database """
+        session = sessionmaker(bind=self.engine)
+        session = session()
 
         for row in session.query(GarminSummaryTable).all():
             gsum = GarminSummary()
-            for sl_ in gsum._db_entries:
+            for sl_ in DB_ENTRIES:
                 setattr(gsum, sl_, getattr(row, sl_))
             self.summary_list[gsum.filename] = gsum
         session.close()
         return self.summary_list
 
     def write_sql_table(self, summary_list):
-        Session = sessionmaker(bind=self.engine)
-        session = Session()
+        """ serialize into database """
+        session = sessionmaker(bind=self.engine)
+        session = session()
 
         slists = []
         def convert_to_sql(sl_):
-            sld = {x: getattr(sl_, x) for x in sl_._db_entries}
+            """ ... """
+            sld = {x: getattr(sl_, x) for x in DB_ENTRIES}
             return GarminSummaryTable(**sld)
 
         for sl_ in summary_list:
+            assert isinstance(sl_, GarminSummary)
             fn_ = sl_.filename
             if fn_ in self.summary_list:
                 sl0 = self.summary_list[fn_]
                 if not all(getattr(sl_, x) == getattr(sl0, x)
-                           for x in sl_._db_entries):
+                           for x in DB_ENTRIES):
                     obj = session.query(GarminSummaryTable)\
                                  .filter_by(filename=fn_).all()[0]
                     session.delete(obj)
@@ -104,5 +112,18 @@ class GarminCacheSQL:
         session.close()
 
     def get_cache_summary_list(self, directory, options=None):
+        """ redirect call """
         return self.garmin_cache.get_cache_summary_list(directory,
                                                         options=options)
+
+
+def write_postgresql_table(summary_list, get_summary_list=False):
+    """ convenience function """
+    with OpenPostgreSQLsshTunnel():
+        from .garmin_cache_sql import GarminCacheSQL
+        postgre_str = POSTGRESTRING + '/garmin_summary'
+        gc_ = GarminCacheSQL(sql_string=postgre_str)
+        sl_ = gc_.read_sql_table()
+        if get_summary_list:
+            return sl_
+        gc_.write_sql_table(summary_list)
