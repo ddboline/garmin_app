019b75ba442724bf9775dd3dd4e01952782a308d
Daniel Boline <ddboline@gmail.com>
2015-09-30 19:35:10 -0400

    ...

--- test_cloud9.sh
+++ test_cloud9.sh
@@ -2,6 +2,8 @@
 
 nosetests --with-coverage --cover-package=garmin_app ./tests/*.py garmin_app/*.py
 
+# pyreverse garmin_app
+# for N in classes packages; do dot -Tps ${N}*.dot > ${N}.ps ; ps2pdf ${N}.ps ; done
 ./garmin.py get
 ./garmin.py year run
 ./garmin.py 2014-11-22_18
