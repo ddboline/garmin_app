d98da7124f357b912bd67dc2a99ec014a940debd
Daniel Boline <ddboline@gmail.com>
2015-10-01 12:11:35 -0400

    ...

--- setup_cloud9.sh
+++ setup_cloud9.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y --force-yes gpsbabel garmin-forerunner-tools xml2 python
                                     python-pandas python-dateutil python-usb fit2tcx \
                                     python-psycopg2 python-sqlalchemy python-nose \
                                     python-coverage python-numpy=1.9\* libtinyxml2.6.2 \
-                                    python-boto
+                                    python-boto python-lxml
 
 if [ -z $1 ] ; then
     true
