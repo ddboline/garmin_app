5059c7adfbfca191ca6ad191892b66ba1b2cf96f
Daniel Boline <ddboline@gmail.com>
2015-10-08 13:51:10 -0400

    ...

--- setup_conda.sh
+++ setup_conda.sh
@@ -5,11 +5,8 @@ sudo chown -R root:root /root/
 sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/devel ./ > /etc/apt/sources.list.d/py2deb2.list"
 sudo apt-get update
 sudo apt-get install -y --force-yes gpsbabel garmin-forerunner-tools xml2 fit2tcx libtinyxml2.6.2
-sudo /opt/conda/bin/conda install --yes pip requests pandas dateutil matplotlib boto psycopg2 sqlalchemy nose
-
-sudo /opt/conda/bin/pip install --upgrade pyusb
-### weird side effect of using pip (font cache for matplotlib needs this directory)
-sudo chown -R ubuntu:ubuntu /home/ubuntu/.cache
+sudo /opt/conda/bin/conda install -c https://conda.anaconda.org/ddboline --yes requests pandas \
+        dateutil matplotlib boto psycopg2 sqlalchemy nose stravalib pyusb
 
 if [ -z $1 ] ; then
     true
