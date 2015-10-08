5e9e1bd0cb443811537b87fc76670442f558ed6d
Daniel Boline <ddboline@gmail.com>
2015-10-08 13:29:54 -0400

    ...

--- setup_conda.sh
+++ setup_conda.sh
@@ -4,7 +4,7 @@ sudo cp -a ${HOME}/.ssh /root/
 sudo chown -R root:root /root/
 sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/devel ./ > /etc/apt/sources.list.d/py2deb2.list"
 sudo apt-get update
-sudo apt-get install -y --force-yes gpsbabel garmin-forerunner-tools xml2 fit2tcx
+sudo apt-get install -y --force-yes gpsbabel garmin-forerunner-tools xml2 fit2tcx libtinyxml2.6.2
 sudo /opt/conda/bin/conda install --yes pip requests pandas dateutil matplotlib boto psycopg2 sqlalchemy nose
 
 sudo /opt/conda/bin/pip install --upgrade pyusb
