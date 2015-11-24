9462490e08976def503cdf4fee6e3c4931ddcd08
Daniel Boline <ddboline@gmail.com>
2015-11-24 10:29:38 -0500

    finish initial implementation, add unit test

--- garmin_app/garmin_corrections_sql.py
+++ garmin_app/garmin_corrections_sql.py
@@ -6,32 +6,16 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-from sqlalchemy import (create_engine, Column, Integer, Float, DateTime,
-                        ForeignKey)
+from sqlalchemy import (create_engine, Column, Integer, Float, DateTime)
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import sessionmaker
 
 from .util import OpenPostgreSQLsshTunnel, POSTGRESTRING
-from .garmin_corrections import (LIST_OF_MISLABELED_TIMES,
-                                 get_list_of_corrected_laps)
+from .garmin_corrections import DB_ENTRIES
+from .garmin_utils import print_date_string
 
 Base = declarative_base()
 metadata = Base.metadata
-
-
-class GarminCorrectionsStarts(Base):
-    """ ORM class for garmin_corrections_starts table """
-    __tablename__ = 'garmin_corrections_starts'
-
-    id = Column(Integer, primary_key=True, unique=True)
-    start_time = Column(DateTime, unique=True)
-    distance = Column(Float)
-    duration = Column(Float)
-
-    def __repr__(self):
-        return 'GarminCorrectionsStarts<%s>' % ', '.join(
-            '%s=%s' % (x, getattr(self, x)) for x in ('id', 'start_time',
-                                                      'distance', 'duration'))
 
 
 class GarminCorrectionsLaps(Base):
@@ -39,18 +23,15 @@ class GarminCorrectionsLaps(Base):
     __tablename__ = 'garmin_corrections_laps'
 
     id = Column(Integer, primary_key=True, unique=True)
-    start_time = Column(DateTime,
-                        ForeignKey('garmin_corrections_starts.start_time'))
+    start_time = Column(DateTime)
     lap_number = Column(Integer)
     distance = Column(Float)
     duration = Column(Float)
 
-    start_time_rel = relationship('GarminCorrectionsStarts')
-
     def __repr__(self):
         return 'GarminCorrectionsLaps<%s>' % ', '.join(
-            '%s=%s' % (x, getattr(self, x)) for x in ('start_time', 'distance',
-                                                      'duration'))
+            '%s=%s' % (x, getattr(self, x)) for x in (
+                'start_time', 'lap_number', 'distance', 'duration'))
 
 
 class GarminCorrectionsSQL(object):
@@ -60,13 +41,8 @@ class GarminCorrectionsSQL(object):
         if garmin_corrections_ is not None:
             self.garmin_corrections_ = garmin_corrections_
         else:
-            self.garmin_corrections_ = get_list_of_corrected_laps()
+            self.garmin_corrections_ = {}
         self.sql_string = sql_string
-        self.summary_list = {}
-        if isinstance(summary_list, dict):
-            self.summary_list = summary_list
-        elif isinstance(summary_list, list):
-            self.summary_list = {v.filename: v for v in summary_list}
 
         self.engine = create_engine(self.sql_string, echo=False)
         self.create_table()
@@ -83,15 +59,25 @@ class GarminCorrectionsSQL(object):
         """ deserialize from database """
         session = sessionmaker(bind=self.engine)
         session = session()
-        with session.begin():
-            for row in session.query(GarminCorrectionsStarts).all():
-                gsum = GarminCorrectionsStarts()
-                for sl_ in DB_ENTRIES:
-                    setattr(gsum, sl_, getattr(row, sl_))
-                self.summary_list[gsum.filename] = gsum
-        return self.summary_list
+        for row in session.query(GarminCorrectionsLaps).all():
+            gsum = GarminCorrectionsLaps()
+            for sl_ in DB_ENTRIES:
+                setattr(gsum, sl_, getattr(row, sl_))
+            key = print_date_string(gsum.start_time)
+            lap = gsum.lap_number
+            dur = gsum.duration
+            dis = gsum.distance
+            if key not in self.garmin_corrections_:
+                self.garmin_corrections_[key] = {}
+            if dur >= 0:
+                self.garmin_corrections_[key][lap] = [dis, dur]
+            else:
+                self.garmin_corrections_[key][lap] = dis
+        session.commit()
+        session.close()
+        return self.garmin_corrections_
 
-    def write_sql_table(self, summary_list):
+    def write_sql_table(self, corrections):
         """ serialize into database """
         session = sessionmaker(bind=self.engine)
         session = session()
@@ -101,50 +87,75 @@ class GarminCorrectionsSQL(object):
         def convert_to_sql(sl_):
             """ ... """
             sld = {x: getattr(sl_, x) for x in DB_ENTRIES}
-            return GarminSummaryTable(**sld)
+            return GarminCorrectionsLaps(**sld)
 
-        for fn_, sl_ in summary_list.items():
-            if not isinstance(sl_, GarminSummary):
-                print(type(sl_))
-            assert isinstance(sl_, GarminSummary)
-            fn_ = sl_.filename
-            if fn_ in self.summary_list:
-                sl0 = self.summary_list[fn_]
-                if not all(getattr(sl_, x) == getattr(sl0, x)
-                           for x in DB_ENTRIES):
-                    obj = session.query(GarminSummaryTable)\
-                                 .filter_by(filename=fn_).all()[0]
-                    session.delete(obj)
-                    slists.append(convert_to_sql(sl_))
-            else:
-                slists.append(convert_to_sql(sl_))
+        id_ = 0
+        q = session.query(GarminCorrectionsLaps).order_by(
+            GarminCorrectionsLaps.id.desc()).first()
+        if q:
+            id_ += q.id + 1
+
+        for st_, cor in corrections.items():
+            for key, val in cor.items():
+                if isinstance(val, list):
+                    dis = val[0]
+                    dur = val[1]
+                else:
+                    dis = val
+                    dur = -1
+
+                if st_ == 'DUMMY':
+                    continue
+                slists.append(GarminCorrectionsLaps(id=id_, start_time=st_,
+                                                    lap_number=key,
+                                                    distance=dis,
+                                                    duration=dur))
+                id_ += 1
 
         session.add_all(slists)
         session.commit()
         session.close()
 
-    def get_cache_summary_list(self, directory, options=None):
-        """ redirect call """
-        return self.garmin_corrections_.get_cache_summary_list(directory,
-                                                               options=options)
 
-
-def write_postgresql_table(summary_list, get_summary_list=False,
-                           dbname='garmin_summary'):
+def write_postgresql_table(corrections, dbname='garmin_summary'):
     """ convenience function """
     with OpenPostgreSQLsshTunnel(port=5433) as pport:
-        return _write_postgresql_table(summary_list,
-                                       get_summary_list=get_summary_list,
-                                       dbname=dbname, port=pport)
+        return _write_postgresql_table(corrections, dbname=dbname, port=pport)
 
 
-def _write_postgresql_table(summary_list, get_summary_list=False,
-                            dbname='garmin_summary', port=5432):
+def _write_postgresql_table(corrections, dbname='garmin_summary', port=5432):
     """ ... """
-    from .garmin_cache_sql import GarminCacheSQL
     postgre_str = '%s:%d/%s' % (POSTGRESTRING, port, dbname)
-    gc_ = GarminCacheSQL(sql_string=postgre_str)
+    gc_ = GarminCorrectionsSQL(sql_string=postgre_str)
     sl_ = gc_.read_sql_table()
-    if not get_summary_list:
-        gc_.write_sql_table(summary_list)
+    gc_.write_sql_table(corrections)
     return sl_
+
+
+def read_postgresql_table(dbname='garmin_summary'):
+    with OpenPostgreSQLsshTunnel(port=5433) as pport:
+        return _read_postgresql_table(dbname=dbname, port=pport)
+
+
+def _read_postgresql_table(dbname='garmin_summary', port=5432):
+    postgre_str = '%s:%d/%s' % (POSTGRESTRING, port, dbname)
+    gc_ = GarminCorrectionsSQL(sql_string=postgre_str)
+    return gc_.read_sql_table()
+
+
+def test_garmin_corrections_sql():
+    from .garmin_corrections import list_of_corrected_laps
+    cor0 = list_of_corrected_laps()
+    write_postgresql_table(cor0, dbname='test_garmin_summary')
+    cor1 = read_postgresql_table(dbname='test_garmin_summary')
+
+    for key, val in cor0.items():
+        if key == 'DUMMY':
+            continue
+        for lap in val:
+            print(key, lap, cor0[key][lap], cor1[key][lap])
+            if isinstance(val[lap], list):
+                assert abs(cor0[key][lap][0] - cor1[key][lap][0]) < 1e-6
+                assert abs(cor0[key][lap][1] - cor1[key][lap][1]) < 1e-6
+            else:
+                assert abs(cor0[key][lap] - cor1[key][lap]) < 1e-6
