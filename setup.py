35c8de71288f73b206ba4bb1f2d2b44753682801
Daniel Boline <ddboline@gmail.com>
2015-11-27 20:00:59 -0500

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name='garmin_app',
-    version='0.0.4.2',
+    version='0.0.4.3',
     author='Daniel Boline',
     author_email='ddboline@gmail.com',
     description='garmin_app',
