3e0ed434d5555cbd949e0ef63a08fb390ea01ece
Daniel Boline <ddboline@gmail.com>
2015-12-14 16:09:30 -0500

    pyusb

--- setup_python3.sh
+++ setup_python3.sh
@@ -6,7 +6,7 @@ export LANG="C.UTF-8"
 sudo bash -c "echo deb ssh://ddboline@ddbolineathome.mooo.com/var/www/html/deb/trusty/python3/devel ./ > /etc/apt/sources.list.d/py2deb2.list"
 sudo apt-get update
 sudo apt-get install -y --force-yes gpsbabel garmin-forerunner-tools xml2 python3-requests \
-                                    python3-pandas python3-dateutil python3-pyusb fit2tcx \
+                                    python3-pandas python3-dateutil python3-usb fit2tcx \
                                     python3-psycopg2 python3-sqlalchemy python3-matplotlib \
                                     python3-nose python3-coverage python3-numpy=1.\* \
                                     python3-boto python3-stravalib 
