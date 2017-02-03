2f04c17c1fe4cedd4208bc29e9df6bf3b82605ca
Daniel Boline <ddboline@gmail.com>
2017-02-03 03:55:21 -0500

    nosetests -> py.test3

--- test_python3.sh
+++ test_python3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-nosetests --with-coverage --cover-package=garmin_app ./tests/*.py garmin_app/*.py
+py.test3 --with-coverage --cover-package=garmin_app ./tests/*.py garmin_app/*.py
 
 # rm -rf ${HOME}/.garmin_cache/
 # python3 ./garmin.py get
