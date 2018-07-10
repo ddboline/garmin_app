e532ed42d26bd1789e6833e0c0ffe83e088cd1c5
Daniel Boline <ddboline@gmail.com>
2018-07-10 06:02:26 -0400

    want to move to python3 only

--- setup.py
+++ setup.py
@@ -12,11 +12,10 @@ from setuptools import setup
 console_scripts = (('garmin-app', 'garmin_app.garmin_utils:main'),
                    ('strava-upload', 'garmin_app.strava_upload:strava_upload'))
 
-if sys.version_info.major == 2:
-    console_scripts = ['%s = %s' % (x, y) for x, y in console_scripts]
-else:
-    v = sys.version_info.major
-    console_scripts = ['%s%s = %s' % (x, v, y) for x, y in console_scripts]
+console_scripts = ['%s = %s' % (x, y) for x, y in console_scripts]
+
+v = sys.version_info.major
+console_scripts.extend('%s%s = %s' % (x, v, y) for x, y in console_scripts)
 
 setup(
     name='garmin_app',
